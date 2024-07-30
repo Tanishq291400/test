d577fa9fd20f318ce1cf2c26402b1405736ea744
tanishq291400 <ptanishq2914@gmail.com>
2024-07-30 23:15:57 +0530

    first commit

---
+++ bomb.py
@@ -0,0 +1,103 @@
+x=23
+y=23.22
+x+y
+print(x+y)
+text="hello bro"
+print('text')
+
+print(type(x))
+
+a=20
+b=10
+if(a<=10 and b>=20):
+     print("And operator correct")
+elif(a<=10 or b>=20):
+     print("Or operator correct")
+else:
+     print("Nothing is going right")
+
+print(a is not b)
+
+a=20
+if(a%2==0):
+     print("Even number")
+elif(a%2==-1):
+     print("Dont know ")
+else:
+     print("Odd")
+"""
+"""
+baby=[1,25,45,3,10,3]
+"""
+#print first 5 elements
+#print elements from 3 to 6
+#print elements with even number of elements
+"""
+#print(baby[0:5])
+#print(baby[2:5])
+#print(baby[0:6:3])
+for i in range(10):
+    if baby[i]%2==0:
+        print("Even number")
+    elif baby[i]%2==-1:
+        print("Invalid")
+    else:
+        print("Odd number")
+dict={'name':'jayaram','age':'18','car':'honda','hotel':'trivago','gender':'male'}
+#print(dict)
+#dict.values()
+"""
+baby=[i**3 for i in range(0,6)]
+print([baby])
+"""
+x=23
+y=23.22
+x+y
+print(x+y)
+text="hello bro"
+print('text')
+
+print(type(x))
+
+a=20
+b=10
+if(a<=10 and b>=20):
+     print("And operator correct")
+elif(a<=10 or b>=20):
+     print("Or operator correct")
+else:
+     print("Nothing is going right")
+
+print(a is not b)
+print
+a=20
+if(a%2==0):
+     print("Even number")
+elif(a%2==-1):
+     print("Dont know ")
+else:
+     print("Odd")
+"""
+"""
+baby=[1,25,45,3,10,3]
+"""
+#print first 5 elements
+#print elements from 3 to 6
+#print elements with even number of elements
+"""
+#print(baby[0:5])
+#print(baby[2:5])
+#print(baby[0:6:3])
+for i in range(10):
+    if baby[i]%2==0:
+        print("Even number")
+    elif baby[i]%2==-1:
+        print("Invalid")
+    else:
+        print("Odd number")
+dict={'name':'Tanishq','age':'18','car':'honda','hotel':'trivago','gender':'male'}
+#print(dict)
+#dict.values()
+baby=[i**3 for i in range(0,6)]
+print(Tanishq PATIL);
+
